561285b7bb7c2ee4813a1663ac34273183ebed49
Y@ng <69349580+AntonYad@users.noreply.github.com>
2021-04-27 17:32:37 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,34 +1,17 @@
-#include<semaphore.h>
-#include<unistd.h>
-class H2O {
+class Solution {
 public:
-	H2O() {
-		sem_init(&h, 0, 2);
-		sem_init(&o, 0, 1);
-	}
-
-	void hydrogen(function<void()> releaseHydrogen) {
-		sem_wait(&h);
-		// releaseHydrogen() outputs "H". Do not change or remove this line.
-		releaseHydrogen();
-		count++;
-		if (count == 2)
+	void rotate(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+		vector<vector<int>> res(n, vector<int>(n, 0));
+		int len = n - 1;
+		for (int i = 0; i<n; i++)
 		{
-			count = 0;
-			sem_post(&o);
+			for (int j = 0; j<n; j++)
+			{
+				res[j][len] = matrix[i][j];
+			}
+			len -= 1;
 		}
+		matrix = res;
 	}
-
-	void oxygen(function<void()> releaseOxygen) {
-		usleep(1000);
-		sem_wait(&o);
-		// releaseOxygen() outputs "O". Do not change or remove this line.
-		releaseOxygen();
-		sem_post(&h);
-		sem_post(&h);
-	}
-private:
-	int count = 0;
-	sem_t h;
-	sem_t o;
 };
