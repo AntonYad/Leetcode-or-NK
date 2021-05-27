c22f14d396e25595ecff963f49ac1e0fa80b21a3
AntonYad <651299847@qq.com>
2021-05-28 06:37:10 +0800

    几位数

---
+++ 奇数位/源.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void oddInOddEvenInEven(vector<int>& arr, int len) {
+        int i = 0, j = 1;
+        while (i < len && j < len) {
+            if (arr[len - 1] % 2 == 0) {
+                swap(arr[len - 1], arr[i]);
+                i += 2;
+            }
+            else {
+                swap(arr[len - 1], arr[j]);
+                j += 2;
+            }
+        }
+    }
+};
