e9f52e9c03c69e3716741794b7622798dd299fed
AntonYad <651299847@qq.com>
2021-06-03 07:53:49 +0800

    rabbit

---
+++ NKrabbit/源.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	long long a[100];
+	a[1] = 1;
+	a[2] = 2;
+	while (cin >> n) {
+		for (int i = 3; i <= n; i++) {
+			a[i] = a[i - 1] + a[i - 2];
+		}
+		cout << a[n] << endl;
+	}
+}
