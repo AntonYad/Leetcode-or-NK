6919ef96fc58771a90e780def7a32a45683892be
AntonYad <651299847@qq.com>
2021-06-06 08:04:17 +0800

    lc876

---
+++ lc876/源.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+
+        ListNode* slow = head;
+        while ((fast->next) && (fast->next->next))
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (fast->next)
+        {
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
